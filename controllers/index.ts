ae2e306e8c6528fe459f7e8165c4d1f991f54c78
coderRaj07 <12131501.edu@gmail.com>
2024-04-22 09:47:26 +0530

    Refactor: Update VandorRoute to include login, profile, and service endpoints

--- controllers/index.ts
+++ controllers/index.ts
@@ -1,2 +1,2 @@
-// export *  from "./VandorController"
+export *  from "./VandorController"
 export *  from "./AdminController"
--- routes/VandorRoute.ts
+++ routes/VandorRoute.ts
@@ -1,8 +1,13 @@
 import express, { Request, Response, NextFunction } from 'express';
-import { CreateVandor } from '../controllers';
+import { UpdateVandorProfile, GetVandorProfile, UpdateVandorService, VandorLogin } from '../controllers';
+import { Authenticate } from '../middlewares/commonAuth';
 
 const router = express.Router();
 
+router.post("/login", VandorLogin)
+router.get("/profile",Authenticate, GetVandorProfile)
+router.patch("/profile",Authenticate, UpdateVandorProfile)
+router.patch("/service",Authenticate, UpdateVandorService)
 
 router.get("/", (req: Request, res: Response, next: NextFunction) => {
     res.json({ message: "Hello From Vandor" });
