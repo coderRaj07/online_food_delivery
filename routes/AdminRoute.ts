e3e9289586c114686940fdb25319a142ad88c466
coderRaj07 <12131501.edu@gmail.com>
2024-04-22 07:22:56 +0530

    Fix: typo in AdminRoute endpoint URL fixed

--- routes/AdminRoute.ts
+++ routes/AdminRoute.ts
@@ -5,7 +5,7 @@ import { CreateVandor,GetVandors,GetVandorByID } from "../controllers"
 const router = express.Router();
 
 router.post("/vandor", CreateVandor)
-router.get("/vandor", GetVandors)
+router.get("/vandors", GetVandors)
 router.get("/vandor/:id", GetVandorByID)
 
 
