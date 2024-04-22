4aed05631e28f093856edd02bd97d469166eaad2
coderRaj07 <12131501.edu@gmail.com>
2024-04-22 07:22:23 +0530

    Refactor: code redundancy handled by utility function and other functioanlities added

--- controllers/AdminController.ts
+++ controllers/AdminController.ts
@@ -4,10 +4,18 @@ import { Vandor } from "../models";
 import { GeneratePassword, GenerateSalt } from "../utility";
 //Admin Creates Vandor
 
+export const FindVandor = async (id: string | undefined, email?: string) => {
+    if (email) {
+        return await Vandor.findOne({ email });
+    }
+    else {
+        return await Vandor.findById(id);
+    }
+}
 
 export const CreateVandor = async (req: Request, res: Response, next: NextFunction) => {
     const { name, address, pincode, foodType, ownerName, phone, email, password } = <CreateVandorInput>req.body;
-    const existingVandor = await Vandor.findOne({ email });
+    const existingVandor = await FindVandor(undefined, email);
 
     if (existingVandor) {
         return res.status(400).json({ message: "Vandor already exists" });
@@ -39,9 +47,19 @@ export const CreateVandor = async (req: Request, res: Response, next: NextFuncti
 }
 
 export const GetVandors = async (req: Request, res: Response, next: NextFunction) => {
-    res.json({ message: "Create Vandor" });
+    const vandors = await Vandor.find({});
+    if (!vandors) {
+        return res.status(404).json({ message: "No Vandors Found" });
+    }
+    return res.json(vandors);
 }
 
 export const GetVandorByID = async (req: Request, res: Response, next: NextFunction) => {
-    res.json({ message: "Create Vandor" });
+    const { id } = req.params;
+    const vandor = await FindVandor(id);
+    if (!vandor) {
+        return res.status(404).json({ message: "No Vandor Found" });
+    }
+    return res.json(vandor);
+
 }
